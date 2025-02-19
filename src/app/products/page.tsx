fcbc4b3f7cd07a536c73c421197d77e8e89f31b7
Mateus_2020 Cerqueira <mateus.cerq7@outlook.com>
2025-02-19 08:40:06 -0300

    chore: add eslint sort plugin and prettier plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,8 +3,8 @@ import { Input } from "@/components/ui/input";
 
 const ProductPage = () => {
   return (
-    <div className="p-5  border border-red-500 rounded-xl">
-      <h1 className="text-red-500">Product Page</h1>
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500 p-3">Product Page</h1>
       <Button>Teste</Button>
       <Input placeholder="teste" />
     </div>
